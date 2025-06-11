727f181ed0008ee88e80ac5ec3f3d33c8512ac8a
dolkersonam <sdolker548@gmail.com>
2025-06-11 19:26:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection" , (socket)=>{
+    socket.on("user_message" , (message) => {
+        socket.emit("message", { text: message, type: "sent" });
+
+        socket.broadcast.emit("message", { text: message, type: "received" });
+    })
+})
+
+app.use(express.static(path.resolve('./public')));
+
+app.get('/' , (req, res)=>{
+    return res.sendFile('./public/index.html');
+})
+server.listen(9000, ()=> console.log("Server started at PORT : 9000"));
+
